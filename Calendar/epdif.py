5307f47c1bf8fda9c1e467158d2ffedf364f049c
dkudlek <david.kudlek@goliath-sw.com>
2019-08-03 10:03:06 +0200

    TASK: Enable cross platform development and simplify serialization
    Added pathlib:
    - Allows easier cross platform development by avoiding (back)slash issuses
    Remove GPIO dependancy from e_paper_drivers
    Simplify serialization and integrate new interface into E-Paper.py
    Replace etc/timezone with tzlocal

--- Calendar/epdif.py
+++ Calendar/epdif.py
@@ -33,4 +33,39 @@ def epd_init():
     GPIO.setup(BUSY_PIN, GPIO.IN)
     SPI.max_speed_hz = 2000000
     SPI.mode = 0b00
-    return 0;
+    return 0
+
+def is_busy():
+    val = epd_digital_read(BUSY_PIN)
+    if val == 0:
+        return True
+    else:
+        return False
+
+def reset_low():
+    epd_digital_write(RST_PIN, GPIO.LOW)
+
+def reset_high():
+    epd_digital_write(RST_PIN, GPIO.HIGH)
+
+def send_command(command):
+    epd_digital_write(DC_PIN, GPIO.LOW)
+    # the parameter type is list but not int
+    # so use [command] instead of command
+    spi_transfer([command])
+
+def send_data(data):
+    epd_digital_write(DC_PIN, GPIO.HIGH)
+    # the parameter type is list but not int
+    # so use [data] instead of data
+    spi_transfer([data])
+
+def epd_exit():
+    print("spi end")
+    SPI.close()
+
+    print("close 5V, Module enters 0 power consumption ...")
+    GPIO.output(RST_PIN, 0)
+    GPIO.output(DC_PIN, 0)
+
+    GPIO.cleanup()
