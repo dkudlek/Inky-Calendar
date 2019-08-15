39676bdd0db209a4b35aac92621a0ca192bd1104
David Kudlek <david.kudlek@goliath-sw.com>
2019-08-15 15:59:01 +0200

    TASK: Remove calibration.py
    Integrate calibration into e_paper_drivers
    Replace red, white, black image files with image files generated by Pillow

--- Calendar/calibration.py
+++ Calendar/calibration.py
@@ -9,23 +9,25 @@ ghosting.
 from __future__ import print_function
 import time
 from settings import display_colours
-from image_data import black, white, red
+from PIL import Image
 
 def calibration():
     """Function for Calibration"""
     import e_paper_drivers
     epd = e_paper_drivers.EPD()
     print('_________Calibration for E-Paper started_________'+'\n')
-
+    black_img = Image.new('RGB', (epd.width, epd.height), "black")
+    red_img = Image.new('RGB', (epd.width, epd.height), "red")
+    white_img = Image.new('RGB', (epd.width, epd.height), "white")
     for i in range(2):
         epd.init()
         print('Calibrating black...')
-        epd.display_image(black)
+        epd.display_image(black_img)
         if display_colours == "bwr":
             print('calibrating red...')
-            epd.display_image(red)
+            epd.display_image(red_img)
         print('Calibrating white...')
-        epd.display_image(white)
+        epd.display_image(white_img)
         epd.sleep()
         print('Cycle', str(i+1)+'/2', 'complete'+'\n')
     print('Calibration complete')
